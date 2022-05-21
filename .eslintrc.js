007f5626fb789ddf047406dfcd02c097f8423724
nekusu <nekusu.dev@gmail.com>
2022-05-20 23:39:38 -0300

    Install ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es2021": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+        'react/prop-types': [0],
+        "react/react-in-jsx-scope": "off",
+    }
+}
